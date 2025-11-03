feaf7b094d58c47c4a98e29952627c0ee40d07ac
isinha <barbosaisabella101085@gmail.com>
2025-11-03 18:57:03 -0300

    Inicializa repositório e adiciona projeto Playwright

---
+++ aulas.sql
@@ -0,0 +1,8 @@
+INSERT INTO propostas (secretaria, status, valor, origem)
+VALUES
+  ('Saúde', 'Aprovado', 150000.00, 'Governo Federal'),
+  ('Educação', 'Em Análise', 75000.00, 'Governo Estadual'),
+  ('Transporte', 'Rejeitado', 50000.00, 'Governo Municipal'),
+  ('Segurança', 'Aprovado', 120000.00, 'Governo Federal'),
+  ('Meio Ambiente', 'Em Análise', 30000.00, 'ONGs');
+
